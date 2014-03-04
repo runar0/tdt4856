9e6d6dedfe79b49beef0c01e5fe151af8b51e72d
Runar B. Olsen <runar.b.olsen@gmail.com>
2014-03-04 15:35:21 +0100

    Restructured vlcplayer project, now using mavenLCPlayer.java -> src/main/java/no/ntnu/eit/skeis/vlcplayer/VLCPlayer.java

---
+++ vlcplayer/src/main/java/no/ntnu/eit/skeis/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.eit.skeis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
